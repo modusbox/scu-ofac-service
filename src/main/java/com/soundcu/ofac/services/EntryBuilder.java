b9ce9d9d1b9c5d548fb7f23433e9992bc4373d5c
Ramiro González Maciel <ramirogm@gmail.com>
2023-03-08 08:27:31 -0300

    Use a local cache to bootstrap the service

--- src/main/java/com/soundcu/ofac/services/EntryBuilder.java
+++ src/main/java/com/soundcu/ofac/services/EntryBuilder.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.io.IOException;
 import java.net.URI;
@@ -19,6 +20,9 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Arrays;
 import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
@@ -30,6 +34,9 @@ public class EntryBuilder {
   @Value("${entries.host}")
   private String entriesHost;
 
+  @Value("${entries.local}")
+  private String entriesLocal;
+
   private List<Entry> entries;
   private List<SourceMetaData> sources;
 
@@ -39,30 +46,56 @@ public class EntryBuilder {
    */
   @Scheduled(fixedRate = 60000 * 60)
   public void fetchEntries() {
-    HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build(); // follow redirects
-    HttpRequest request = HttpRequest.newBuilder(URI.create(entriesHost))
-        .header("Accept", "application/json")
-        .build();
-    try {
-			logger.info("Starting fetching entries...");
-      HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-      if (response.statusCode() != 200) {
-        logger.error("error fetching entries. Status code:: " + response.statusCode());
-        return;
-      }
-      logger.info("Finished fetching entries");
-      logger.info("Staring JSON mapping");
-      JSONObject entriesObj = new JSONObject(response.body());
-      JSONArray array = entriesObj.getJSONArray(RESULTS);
-      entries = Arrays.asList(mapper.readValue(array.toString(), Entry[].class));
-      array = entriesObj.getJSONArray(SOURCES);
-      sources = Arrays.asList(mapper.readValue(array.toString(), SourceMetaData[].class));
-      logger.info("Finished JSON mapping");
-    } catch (Exception e) {
-      logger.error("Exception#fetchEntries ", e);
+    if ( entries == null ) {
+      this.fetchEntries(entriesLocal);
+    } else {
+      this.fetchEntries(entriesHost);
     }
   }
 
+  public void fetchEntries(String source) {
+    if ( source.startsWith("http") ) {
+      HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build(); // follow redirects
+      HttpRequest request = HttpRequest.newBuilder(URI.create(source))
+          .header("Accept", "application/json")
+          .build();
+      try {
+        logger.info("Starting fetching entries...");
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+          logger.error("error fetching entries. Status code:: " + response.statusCode());
+          return;
+        }
+        logger.info("Finished fetching entries");
+        this.readEntriesFromSource(response.body());
+      } catch (Exception e) {
+        logger.error("Exception#fetchEntries ", e);
+      }
+    } else if ( source.startsWith("/") ) {
+      try {
+        InputStream is = this.getClass().getResourceAsStream(source);
+        if (is == null) {
+          throw new IOException("Resource not found");
+        }
+        String entriesBody = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        this.readEntriesFromSource(entriesBody);
+      } catch (Exception e) {
+        logger.error("Exception#fetchEntries ", e);
+      }
+    } else {
+      logger.error("entries source not recognized: " + source);
+    }
+  }
+
+  protected void readEntriesFromSource(String entriesBody) throws IOException, JsonProcessingException  {
+    logger.info("Starting JSON mapping");
+    JSONObject entriesObj = new JSONObject(entriesBody);
+    JSONArray array = entriesObj.getJSONArray(RESULTS);
+    entries = Arrays.asList(mapper.readValue(array.toString(), Entry[].class));
+    array = entriesObj.getJSONArray(SOURCES);
+    sources = Arrays.asList(mapper.readValue(array.toString(), SourceMetaData[].class));
+    logger.info("Finished JSON mapping");
+  }
   public synchronized List<Entry> getEntries() {
     if (entries == null)
       fetchEntries();
